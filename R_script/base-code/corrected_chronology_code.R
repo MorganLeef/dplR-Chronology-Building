758e7addb471585a100697c2597f7d910310a833
Morgan Leef <mlleef@mix.wvu.edu>
2018-12-24 23:37:13 -0500

    Finished calculating "R" but something is not right -- need to values to hang closer to 1.0

--- R_script/base-code/corrected_chronology_code.R
+++ R_script/base-code/corrected_chronology_code.R
@@ -18,10 +18,35 @@ years_PCRU<-as.numeric(rownames(BDR_PCRU.crn))
 
 sub_abba<-subset(BDR_ABBA.crn, years_ABBA > 1979 & years_ABBA < 2017)
 sub_pcru<-subset(BDR_PCRU.crn, years_PCRU > 1979 & years_PCRU < 2017)
+sub_year_abba<-as.numeric(rownames(sub_abba))
+sub_year_pcru<-as.numeric(rownames(sub_pcru))
 
 sub_abba$samp.depth<-NULL
 sub_pcru$samp.depth<-NULL
 
-coefficient<-(sd(sub_abba)/(sd(sub_pcru)))
+BDR_abba_std<-apply(sub_abba, 2, sd)
+BDR_pcru_std<-apply(sub_pcru, 2, sd)
 
-meh<-rowVars(sub_abba, suma = NULL, std = TRUE)
+coefficient<-((BDR_abba_std)/(BDR_pcru_std))
+
+mean(sub_pcru$BDPstd)
+
+sub_pcru_mean<-subset(BDR_PCRU.rwi, years_PCRU > 1979 & years_PCRU < 2017)
+mean<-rowMeans(sub_pcru_mean)
+
+quantity<-((sub_pcru)-(mean))
+
+R<-coefficient*quantity
+
+C<-sub_abba-R
+
+plot(BDR_ABBA.crn, add.spline=TRUE, nyrs=20)
+plot(C, main="Beaver Dam Road", add.spline=TRUE, nyrs=20)
+
+plot(sub_year_abba, C$BDRstd, type='o', axes=F, xlab='', ylab='', main= "Beaver Dam Run", col="blue", xlim=c(1980, 2017))
+  axis(1, seq(1980, 2020, by=5))
+  mtext(side=1, "Year", line=2)
+  axis(2, seq(-0.5, 2.0, by=0.5))
+  mtext(side=2, "RWI", line=2)
+  abline(h=1.0, col="black")
+  
