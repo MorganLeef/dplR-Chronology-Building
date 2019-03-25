adb5332c2c1a3861a5163645ad80cae2a6d88547
Morgan Leef <mlleef@mix.wvu.edu>
2019-03-25 10:45:55 -0400

    removed percents r doesnt like

--- R_script/rotholz_plots.R
+++ R_script/rotholz_plots.R
@@ -5,7 +5,24 @@ rm(list=ls())
 library(ggplot2)
 library(magrittr)
 library(ggpubr)
+library(reshape2)
 
-rot<-read.csv("PsuedoRot.csv")
+BDR_rot<-read.csv("BDR.csv")
 
-ggplot(rot, aes(x=Year, y= ))
+#WIP line plot
+ggplot(BDR_rot, aes(x=Year))+
+  geom_line(aes(y=BDR_rot$Rotholz_n))+
+  geom_line(aes(y=BDR_rot$Rotholz_p),linetype = "dashed")+
+  scale_y_continuous(sec.axis = sec_axis(~.*1, name = "Percent of Cores"))+
+  labs(y = "Number of Cores",x = "Year")+
+  theme(legend.position = c(.05,.2))
+
+#Arguement to change xy bounds
+coord_cartesian(ylim = c(0, 100))   
+
+  
+#Bar chart method  
+df<-melt(BDR_rot,id.vars = "Year")
+  
+ggplot(df, aes(x = Year, y = value,fill=variable)) +
+  geom_bar(stat='identity')  
