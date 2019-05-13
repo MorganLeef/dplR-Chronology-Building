f132661071c3eee3b00833c251ffcbb7063feb1a
Morgan Leef <mlleef@mix.wvu.edu>
2019-05-12 20:22:51 -0400

    added regional chronologies

---
+++ R_script/regional_chronologies.R
@@ -0,0 +1,134 @@
+###Regional Chronology Calculations
+
+rm(list=ls())
+library(dplR)
+library(ggplot2)
+library(magrittr)
+library(ggpubr)
+library(reshape2)
+
+
+###Canaan Region (BFT,COR,DRT,FRE,MAL,MVT,RMR,SEW,VFD)
+######################################################
+temp = list.files(pattern="*.rwl")
+for (i in 1:length(temp)) assign(temp[i], read.rwl(temp[i]))
+
+#ABBA
+cv_region<-c("BFT","COR","DRT","FRE","MAL","MVT","RMR","SEW","VFD")
+cv_region_abba<-combine.rwl(list(BFT_ABBA.rwl,COR_ABBA.rwl,DRT_ABBA.rwl,FRE_ABBA.rwl,MAL_ABBA.rwl,MVT_ABBA.rwl,RMR_ABBA.rwl,SEW_ABBA.rwl,VFD_ABBA.rwl))
+
+#Detrend and create a chronology for the ABBA files in the Canaan Region
+cv_abba.rwi <- detrend(rwl = cv_region_abba, method = "ModNegExp")
+cv_abba.crn<- chron(cv_abba.rwi, prefix = "CVR")
+
+years_ABBA<-as.numeric(rownames(cv_abba.crn))
+
+sub_cv_abba<-subset(cv_abba.crn, years_ABBA > 1949 & years_ABBA < 2017)
+
+crn.plot(sub_cv_abba)
+
+#PCRU
+cv_region_pcru<-combine.rwl(list(BFT_PCRU.rwl,COR_PCRU.rwl,DRT_PCRU.rwl,FRE_PCRU.rwl,MAL_PCRU.rwl,MVT_PCRU.rwl,RMR_PCRU.rwl,SEW_PCRU.rwl,VFD_PCRU.rwl))
+
+#Detrend and create a chronology for the PCRU files in the Canaan Region
+cv_pcru.rwi <- detrend(rwl = cv_region_pcru, method = "ModNegExp")
+cv_pcru.crn<- chron(cv_pcru.rwi, prefix = "CVR")
+
+years_ABBA<-as.numeric(rownames(cv_pcru.crn))
+
+sub_cv_pcru<-subset(cv_pcru.crn, years_ABBA > 1949 & years_ABBA < 2017)
+
+crn.plot(sub_cv_pcru)
+
+###Dolly Sods Wilderness
+########################
+
+###ABBA
+#Detrend and create a chronology for DOL ABBA
+DOL_ABBA.rwi <- detrend(rwl = DOL_ABBA.rwl, method = "ModNegExp")
+DOL_ABBA.crn<- chron(DOL_ABBA.rwi, prefix = "DOL")
+
+years_ABBA<-as.numeric(rownames(DOL_ABBA.crn))
+sub_dol_abba<-subset(DOL_ABBA.crn, years_ABBA > 1950 & years_ABBA < 2017)
+
+crn.plot(sub_dol_abba)
+
+###PCRU
+#Detrend and create a chronology for DOL PCRU
+DOL_PCRU.rwi <- detrend(rwl = DOL_PCRU.rwl, method = "ModNegExp")
+DOL_PCRU.crn<- chron(DOL_PCRU.rwi, prefix = "DOL")
+
+years_PCRU<-as.numeric(rownames(DOL_PCRU.crn))
+sub_dol_pcru<-subset(DOL_PCRU.crn, years_PCRU > 1950 & years_PCRU < 2017)
+
+crn.plot(sub_dol_pcru)
+
+###Beaver Dam Run Region
+########################
+###ABBA
+#Detrend and create a chronology for BDR ABBA
+BDR_ABBA.rwi <- detrend(rwl = BDR_ABBA.rwl, method = "ModNegExp")
+BDR_ABBA.crn<- chron(BDR_ABBA.rwi, prefix = "BDR")
+
+years_ABBA<-as.numeric(rownames(BDR_ABBA.crn))
+sub_bdr_abba<-subset(BDR_ABBA.crn, years_ABBA > 1950 & years_ABBA < 2017)
+
+crn.plot(sub_bdr_abba)
+
+###PCRU
+#Detrend and create a chronology for BDR PCRU
+BDR_PCRU.rwi <- detrend(rwl = BDR_PCRU.rwl, method = "ModNegExp")
+BDR_PCRU.crn<- chron(BDR_PCRU.rwi, prefix = "BDR")
+
+years_PCRU<-as.numeric(rownames(BDR_PCRU.crn))
+sub_bdr_pcru<-subset(BDR_PCRU.crn, years_PCRU > 1950 & years_PCRU < 2017)
+
+crn.plot(sub_bdr_pcru)
+
+###Blister Run Swamp Region
+###########################
+###ABBA
+#Detrend and create a chronology for BRS ABBA
+BRS_ABBA.rwi <- detrend(rwl = BRS_ABBA.rwl, method = "ModNegExp")
+BRS_ABBA.crn<- chron(BRS_ABBA.rwi, prefix = "BRS")
+
+years_ABBA<-as.numeric(rownames(BRS_ABBA.crn))
+sub_brs_abba<-subset(BRS_ABBA.crn, years_ABBA > 1950 & years_ABBA < 2017)
+
+crn.plot(sub_brs_abba)
+
+###PCRU
+#Detrend and create a chronology for BRS PCRU
+BRS_PCRU.rwi <- detrend(rwl = BRS_PCRU.rwl, method = "ModNegExp")
+BRS_PCRU.crn<- chron(BRS_PCRU.rwi, prefix = "BRS")
+
+years_PCRU<-as.numeric(rownames(BRS_PCRU.crn))
+sub_brs_pcru<-subset(BRS_PCRU.crn, years_PCRU > 1950 & years_PCRU < 2017)
+
+crn.plot(sub_brs_pcru)
+
+###Marlington Region
+####################
+###ABBA
+#Detrend and create a chronology for MSH ABBA
+MSH_ABBA.rwi <- detrend(rwl = MSH_ABBA.rwl, method = "ModNegExp")
+MSH_ABBA.crn<- chron(MSH_ABBA.rwi, prefix = "MSH")
+
+years_ABBA<-as.numeric(rownames(MSH_ABBA.crn))
+sub_msh_abba<-subset(MSH_ABBA.crn, years_ABBA > 1965 & years_ABBA < 2017)
+
+crn.plot(sub_msh_abba)
+
+###PCRU
+#Detrend and create a chronology for MSH PCRU
+MSH_PCRU.rwi <- detrend(rwl = MSH_PCRU.rwl, method = "ModNegExp")
+MSH_PCRU.crn<- chron(MSH_PCRU.rwi, prefix = "MSH")
+
+years_PCRU<-as.numeric(rownames(MSH_PCRU.crn))
+sub_msh_pcru<-subset(MSH_PCRU.crn, years_PCRU > 1965 & years_PCRU < 2017)
+
+crn.plot(sub_msh_pcru)
+
+
+
+
